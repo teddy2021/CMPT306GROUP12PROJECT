718ff2cfcd46a2777b37e39abd7bae481e3f98f9
Mason <mrd875@usask.ca>
2020-10-16 18:03:44 -0600

    Getting there

---
+++ Assets/AStarDemo/AStar.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class AStar : MonoBehaviour
+{
+    private AStarNode[,] nodeMap;
+
+    public Tilemap walkMap;
+
+    public bool isStatic = true;
+
+    public bool allowDiags = true;
+
+    private void CreateNodeMap()
+    {
+        if (nodeMap != null && isStatic)
+            return;
+
+        walkMap.CompressBounds();
+
+        var bounds = walkMap.cellBounds;
+
+        nodeMap = new AStarNode[bounds.size.x, bounds.size.y];
+
+        // thank you https://bitbucket.org/Sniffle6/tilemaps-with-astar/src/master/Assets/GridManager.cs
+        for (int x = bounds.xMin, i = 0; i < (bounds.size.x); x++, i++)
+        {
+            for (int y = bounds.yMin, j = 0; j < (bounds.size.y); y++, j++)
+            {
+                if (walkMap.HasTile(new Vector3Int(x, y, 0)))
+                {
+                    // we defined the z part of the node to be '0' when the tile is walkable
+                    nodeMap[i, j] = new AStarNode(x, y, 0);
+                }
+                else
+                {
+                    // else it will be '1' when it is not walkable
+                    nodeMap[i, j] = new AStarNode(x, y, 1);
+                }
+            }
+        }
+    }
+
+    public List<Vector2Int> FindPath (Vector2Int start, Vector2Int goal)
+    {
+        CreateNodeMap();
+
+        var columns = nodeMap.GetUpperBound(0) + 1;
+        var rows = nodeMap.GetUpperBound(1) + 1;
+        AStarNode snode = null;
+        AStarNode gnode = null;
+
+        // init all nodes
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                AStarNode node = nodeMap[x, y];
+
+                node.UpdateNeighbors(nodeMap, isStatic, allowDiags);
+                node.H = 0;
+                node.G = 0;
+                node.Parent = null;
+
+                if (x == start.x && y == start.y)
+                    snode = node;
+
+                if (x == goal.x && y == goal.y)
+                    gnode = node;
+            }
+        }
+
+        return null;
+    }
+}
--- Assets/AStarDemo/AStarNode.cs
+++ Assets/AStarDemo/AStarNode.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AStarNode
 {
-    public Vector2Int coords { get; private set; }
+    public Vector3Int coords { get; private set; }
 
     // f is defined by F = G + H
     public double F { get; private set; }
@@ -39,7 +39,100 @@ public class AStarNode
         }
     }
 
-    public AStarNode (Vector2Int loc, double h = 0.0, double g = 0.0)
+    // the valid adjacent nodes
+    public List<AStarNode> Neighbors { get; private set; }
+
+    public AStarNode Parent { get; set; }
+
+    public void UpdateNeighbors (AStarNode[,] walkMap, bool isStatic, bool allowDiags = true)
+    {
+        if (Neighbors != null && isStatic)
+            return;
+
+        Neighbors = new List<AStarNode>();
+        var x = coords.x;
+        var y = coords.y;
+        var z = coords.z;
+        var maxX = walkMap.GetUpperBound(0);
+        var maxY = walkMap.GetUpperBound(1);
+
+        // check -x
+        if (x > 0)
+        {
+            var nnode = walkMap[x - 1, y];
+
+            if (nnode.coords.z == z)
+                Neighbors.Add(nnode);
+        }
+
+        // check +x
+        if (x < maxX)
+        {
+            var nnode = walkMap[x + 1, y];
+
+            if (nnode.coords.z == z)
+                Neighbors.Add(nnode);
+        }
+
+        // check -y
+        if (y < 0)
+        {
+            var nnode = walkMap[x, y - 1];
+
+            if (nnode.coords.z == z)
+                Neighbors.Add(nnode);
+        }
+
+        // check +y
+        if (y < maxY)
+        {
+            var nnode = walkMap[x, y + 1];
+
+            if (nnode.coords.z == z)
+                Neighbors.Add(nnode);
+        }
+
+        if (allowDiags)
+        {
+            // check -x -y
+            if (x > 0 && y > 0)
+            {
+                var nnode = walkMap[x - 1, y - 1];
+
+                if (nnode.coords.z == z)
+                    Neighbors.Add(nnode);
+            }
+
+            // check +x -y
+            if (x < maxX && y > 0)
+            {
+                var nnode = walkMap[x + 1, y - 1];
+
+                if (nnode.coords.z == z)
+                    Neighbors.Add(nnode);
+            }
+
+            // check +x +y
+            if (x < maxX && y < maxY)
+            {
+                var nnode = walkMap[x + 1, y + 1];
+
+                if (nnode.coords.z == z)
+                    Neighbors.Add(nnode);
+            }
+
+            // check -x +y
+            if (x > 0 && y < maxY)
+            {
+                var nnode = walkMap[x - 1, y + 1];
+
+                if (nnode.coords.z == z)
+                    Neighbors.Add(nnode);
+            }
+        }
+    }
+
+    public AStarNode (Vector3Int loc, double h = 0.0, double g = 0.0)
     {
         coords = loc;
 
@@ -47,9 +140,9 @@ public class AStarNode
         G = g;
     }
 
-    public AStarNode (int x, int y, double h = 0.0, double g = 0.0)
+    public AStarNode (int x, int y, int z, double h = 0.0, double g = 0.0)
     {
-        coords = new Vector2Int(x, y);
+        coords = new Vector3Int(x, y, z);
 
         H = h;
         G = g;
