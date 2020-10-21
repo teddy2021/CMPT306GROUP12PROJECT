818f802b719eaa7be7b009598a88459797ca481d
Matthew G <mkgurski@sasktel.net>
2020-10-20 20:04:23 -0600

    Player Movement and Camera scripts added

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Script is attached to player object
+    // Then player objects rigidbody is set to the variable.
+
+    public float moveSpeed;     // variable for movement speed
+    public Rigidbody2D rb;      // variable for players 2d rigibody
+    private Vector2 moveDir;    // vector variable for calculating movement direction (only needs to be a 2dvector)
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Inputs();
+    }
+
+    // FixedUpdate called every fixed-rate frame
+    void FixedUpdate()
+	{ 
+        Move();
+    }
+
+    // handles the players inputs
+    void Inputs()
+	{
+        float X = Input.GetAxisRaw("Horizontal");           // reads left and right inputs
+        float Y = Input.GetAxisRaw("Vertical");             // reads up and down inputs
+        moveDir = new Vector2(X, Y).normalized;             // calculates the desired postion of the players inputs and noramlizes it (making the directional magnitude 1)
+	}
+
+    // moves the player object (the rigibody to be exact)
+    void Move()
+	{
+        rb.velocity = new Vector2(moveDir.x * moveSpeed, moveDir.y * moveSpeed);    // set the players rigibody velocity vector to the moveDirection vecotr (multipying the values by moveSpeed)
+	}
+}
---
+++ Assets/mouseZoom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseZoom : MonoBehaviour
+{
+
+    // attach script to main camera
+
+    public float zoomSpeed = 1;
+    private float targetOrtho;
+    public float smoothSpeed = 2.0f;
+    public float minZoom = 1.0f;
+    public float maxZoom = 20.0f;
+
+    // grab the orthographic size of the camera as it is now
+    void Start()
+    {
+        targetOrtho = Camera.main.orthographicSize;
+    }
+
+    // Recieve scroll wheel input and adjust the camera's orthographicSize according to the direction of the scroll
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if(scroll != 0.0f)
+		{
+            targetOrtho -= scroll * zoomSpeed;
+            targetOrtho = Mathf.Clamp(targetOrtho, minZoom, maxZoom);
+		}
+
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+    }
+}
