da94f764d06d094cdf89906be85f75a40d575b7c
chadmckellar <cdm090@usask.ca>
2020-10-22 01:26:22 -0600

    added some uniteractable enemies
    healthbars broke on import
    
    need different approach to A* for enemies

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float MovementSpeed;
+
+    public int walkDistance = 200;
+    private int currPos = 0;
+    private bool rightDirection = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+
+    }
+
+    void FixedUpdate()
+    {
+        // swap walk direction if goes passed specified distance 'walkDistance'
+        if (currPos > walkDistance)
+        {
+            rightDirection = false;
+        }
+        if (currPos < 0)
+        {
+            rightDirection = true;
+        }
+
+        // move sprite
+        if (rightDirection)
+        {
+            transform.position += new Vector3(1, 0, 0) * Time.fixedDeltaTime* MovementSpeed;
+            currPos += 1;
+        }
+        else
+        {
+            transform.position += new Vector3(-1, 0, 0) * Time.fixedDeltaTime * MovementSpeed;
+            currPos -= 1;
+        }
+    }
+}
