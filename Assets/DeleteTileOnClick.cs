c7666b39c383ba68d50e88a65e52b58dd91088cc
Mason <mrd875@usask.ca>
2020-11-04 21:57:33 -0600

    Fixed the lighting

---
+++ Assets/DeleteTileOnClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DeleteTileOnClick : MonoBehaviour
+{
+    new Camera camera;
+
+    public Tilemap tilemap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 world = camera.ScreenToWorldPoint(Input.mousePosition);
+
+
+            tilemap.SetTile(tilemap.WorldToCell(world), null);
+        }
+    }
+}
