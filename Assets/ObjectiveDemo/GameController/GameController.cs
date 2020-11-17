e015814485e6ddc801b850e90c9af30ea9d66365
Mason <mrd875@usask.ca>
2020-11-17 15:30:41 -0600

    Added Objective Demo

---
+++ Assets/ObjectiveDemo/GameController/GameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public TextMeshProUGUI text;
+
+    public void OnGameEnded(string reason)
+    {
+        text.text = reason;
+    }
+}
---
+++ Assets/ObjectiveDemo/Objective_Key/Key.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    void OnGrabAnimComplete()
+    {
+        gameObject.GetComponent<SpriteRenderer>().enabled = false; // hide the key after the anim is done
+        transform.GetChild(0).GetComponent<Animator>().Play("Light_Fade"); // fade the light
+    }
+
+    public bool grabbed = false;
+
+    // frame when an entity collides with us
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        var anim = gameObject.GetComponent<Animator>();
+
+        // check if it is the player
+        if (col.gameObject.tag == "Player" && !grabbed /*anim.GetCurrentAnimatorStateInfo(0).IsName("Key_Idle")*/)
+        {
+            grabbed = true;
+
+            // play the grab animation
+            anim.Play("Key_Grab");
+        }
+    }
+}
