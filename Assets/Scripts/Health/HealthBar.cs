c5141a6fd917265fe9dd83b792c4475a9bb0b903
James Kiehn <jim@quicklinkt.com>
2020-11-18 15:13:00 -0600

    merged alpha

---
+++ Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Text text;
+
+    private int maxhealth;
+
+    public void SetHealth(int health)
+    {
+        text.text = health.ToString() + "/" + maxhealth.ToString();
+        slider.value = health;
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        this.maxhealth = health;
+
+        slider.maxValue = maxhealth;
+        slider.value = maxhealth;
+        text.text = maxhealth.ToString() + "/" + maxhealth.ToString();
+        
+    }
+}
