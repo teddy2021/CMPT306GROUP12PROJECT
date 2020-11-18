ee0ea36a2e60b06aac6c13c1c4b703dc7c9d12fa
chadmckellar <cdm090@usask.ca>
2020-11-18 06:59:28 -0700

    Added attackable enemies
    added to powergrid scene

---
+++ Assets/Scripts/enemyHealth/Hp.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Hp : MonoBehaviour
+{
+    public int health;
+    private int currentHealth;
+
+    public HealthBar healthbar;
+
+    void Start()
+    {
+        currentHealth = health;
+        healthbar.SetMaxHealth(health);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        healthbar.SetHealth(currentHealth);
+
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TakeDamage(4);
+        }
+    }
+
+
+}
