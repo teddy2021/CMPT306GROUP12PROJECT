b306b23ca6d43866f58c687690898531c9fba4e3
Mason <mrd875@usask.ca>
2020-11-18 19:43:03 -0600

    Health bar

--- Assets/SortedAssets/HealthBar/HealthBar.cs
+++ Assets/SortedAssets/HealthBar/HealthBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBarSlime : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
 
     public Slider slider;
