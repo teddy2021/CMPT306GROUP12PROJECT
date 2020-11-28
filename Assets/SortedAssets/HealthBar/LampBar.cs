e8f2b1cf33be24536c06219e89526cc040521a14
chadmckellar <cdm090@usask.ca>
2020-11-28 12:13:37 -0700

    added lamp charge bar beneath healthbar
    Also fixed anchoring related bug that caused healthbar to move when damage was taken

---
+++ Assets/SortedAssets/HealthBar/LampBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LampBar : MonoBehaviour
+{
+
+    public Slider slider;
+    public Image fill;
+
+    private int maxhealth;
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    public void SetMaxHealth(int health)
+    {
+        this.maxhealth = health;
+
+        slider.maxValue = maxhealth;
+
+        SetHealth(health);
+    }
+
+}
