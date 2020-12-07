70442f117eca98863ace3a5b24cb3ec4e8065cae
Mason <mrd875@usask.ca>
2020-12-06 18:58:21 -0600

    Fixed amb cave and added fairy roaming

--- Assets/MapMaking/Tiles/Images and Objects/FaiyrGif/Fairy.cs
+++ Assets/MapMaking/Tiles/Images and Objects/FaiyrGif/Fairy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class Fairy : MonoBehaviour
 {
@@ -9,16 +10,49 @@ public class Fairy : MonoBehaviour
     private AudioSource pickupSound;
     public AudioClip[] pickupSounds;
     public GameObject parent;
+
+    private AIPath aIPath;
+    private AIDestinationSetter ai;
+    private GameObject roamDest;
+
+
+    [Tooltip("How far will roam from its current position")]
+    public float roamDist = 5f;
+
+    [Tooltip("How often will roam to a new location?")]
+    public float roamChangeRate = 5f;
+
+    [Tooltip("How fast is when roaming")]
+    public float roamSpeed = 2.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         pickupSound = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<AudioSource>();
+
+        ai = GetComponentInParent<AIDestinationSetter>();
+        aIPath = GetComponentInParent<AIPath>();
+
+        roamDest = new GameObject();
+        InvokeRepeating("UpdateRoamDest", roamChangeRate, roamChangeRate);
+
+        aIPath.maxSpeed = roamSpeed;
+        ai.target = roamDest.transform;
+    }
+
+    void UpdateRoamDest()
+    {
+        roamDest.transform.Translate(
+            new Vector2(
+                transform.position.x + Random.Range(-roamDist, roamDist),
+                transform.position.y + Random.Range(-roamDist, roamDist)
+            )
+        );
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     private void OnTriggerStay2D(Collider2D collision)
